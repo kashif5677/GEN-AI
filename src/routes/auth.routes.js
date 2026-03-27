f4a49e5b8a52b2388ae5e75b137c121535733887
mdkashif <mdkashifnisar5677@gmail.com>
2026-03-27 14:43:30 +0530

    routes update

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const authRouter = express.Router()
+
+
+/**
+ * @route POST /register
+ * @description register a user
+ * @access public
+ */
+
+module.exports = authRouter
