2f74309eecb29efb289a9045aadd231d3004069c
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-07 23:32:27 +0530

    updating

--- Backend/src/service/ai.service.js
+++ Backend/src/service/ai.service.js
@@ -34,10 +34,7 @@ const interviewReportSchema = z.object({
 
 async function generateinterviewReport({ resume, seldescribe, jobdescribe }) {
 
-    const prompt = `Generate a resume based interview report for a job describe for a candidate.
-                     Resume:${resume}
-                    Self describe:${seldescribe}
-                     Job describe:${jobdescribe}`
+
 
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
