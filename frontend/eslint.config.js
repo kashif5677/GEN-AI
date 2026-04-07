0421d7c3210bc753e40e9d45e01168722eaf774e
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-07 23:55:46 +0530

    adding file

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,12 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
